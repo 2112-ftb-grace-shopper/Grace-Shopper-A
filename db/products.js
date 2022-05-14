d723d0297e0ad3babb2f5602a0ee8f0f44577a3c
Allen McKitrick <mckitricka@gmail.com>
2022-05-14 14:31:13 -0400

    updateProduct

---
+++ db/products.js
@@ -0,0 +1,37 @@
+
+async function updateProduct ({id, name, description, price}) {
+    try {
+        const fields = {};
+        if (name) {
+            fields.name = name
+        }
+        if (description) {
+            fields.description = description
+        }
+        if (id){
+            fields.id = id
+        }
+        if (price){
+            fields.price = price
+        }
+        const setString = Object.keys(fields).map(
+
+            (key, index) => `"${key}"=$${index + 2
+                }`
+        ).join(', ');
+        const { rows: [product] } = await client.query(`
+          UPDATE products
+          SET name=${name},
+          description=${description},
+          price=${price}
+          WHERE id=$1
+          RETURNING *;
+        `, [id, ...Object.values(fields)])
+
+
+
+        return product;
+    } catch (error) {
+        throw error;
+    }
+} 
