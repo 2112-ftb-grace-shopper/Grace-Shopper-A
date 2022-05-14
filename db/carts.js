5b4235168b313479e1916766a3f813a78a33ab4b
Allen McKitrick <mckitricka@gmail.com>
2022-05-14 11:07:20 -0400

    allen1

--- db/carts.js
+++ db/carts.js
@@ -8,5 +8,21 @@ async function getCartItemById(id){
         console.error(error)
         throw error
     }
-
 }
+
+async function updateCart({ id, name, price, description, quantity }) {
+    try { 
+        const { rows: [product] } = await client.query(`UPDATE products
+            SET name=${name},
+            description=${description},
+            price=${price}
+            quantity=${quantity}
+            WHERE id=$1
+            RETURNING *;
+        `, [id])
+        return product;
+    } catch (error) {
+      return error;
+    }
+}
+
