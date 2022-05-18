64215dddd6d6a1fe646f6ae195386f3de4b156e6
jvhill728 <jvhill728@gmail.com>
2022-05-18 18:47:31 -0500

    creating shoppingCart api calls

--- api/shoppingCart.js
+++ api/shoppingCart.js
@@ -7,3 +7,25 @@ routinesRouter.use((req, res, next) => {
     console.log('A request is being made to /shoppingcart');
     next();
 });
+
+shoppingCartRouter.get('/', requireUser, async (req, res, next) => {
+    try {
+        const routines = await getShoppingCartItemsByUser();
+        return res.send(routines)
+    } catch ({ name, message }) {
+        return next({ name, message })
+    }
+
+});
+
+routinesRouter.post('/', requireUser, async (req, res, next) => {
+    try {
+        const creatorId = req.user.id;
+        const { name, goal, isPublic } = req.body;
+        const routine = await createRoutine({ creatorId, isPublic, name, goal });
+        return res.send(routine);
+
+    } catch ({ name, message }) {
+        return next({ name, message });
+    }
+});
