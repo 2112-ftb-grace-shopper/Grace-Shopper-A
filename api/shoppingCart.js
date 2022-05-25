2b887cedf83d03e9440ecdf0e24a2c74182135b3
jvhill728 <jvhill728@gmail.com>
2022-05-25 18:47:46 -0500

    shoppingCartRouter patch complete

--- api/shoppingCart.js
+++ api/shoppingCart.js
@@ -2,6 +2,8 @@ const express = require('express');
 const shoppingCartRouter = express.Router();
 const { requireUser } = require('./utils');
 const { getShoppingCartItemsByUser } = require('../db/models/shoppingCart');
+const { default: createBreakpoints } = require('@material-ui/core/styles/createBreakpoints');
+const { createProduct, getProductById, updateProduct } = require('../db');
 
 shoppingCartRouter.use((req, res, next) => {
     console.log('A request is being made to /shoppingcart');
@@ -18,14 +20,37 @@ shoppingCartRouter.get('/', requireUser, async (req, res, next) => {
 
 });
 
-routinesRouter.post('/', requireUser, async (req, res, next) => {
+shoppingCartRouter.post('/', requireUser, async (req, res, next) => {
     try {
-        const creatorId = req.user.id;
-        const { name, goal, isPublic } = req.body;
-        const routine = await createRoutine({ creatorId, isPublic, name, goal });
-        return res.send(routine);
+        const userId = req.user.id;
+        const { make, model, year, color, isAdmin } = req.body;
+        const routine = await createProduct({ userId, isAdmin, make, model, year,
+        color });
+        return res.send(product);
 
-    } catch ({ name, message }) {
-        return next({ name, message });
+    } catch (error) {
+        return next(error);
     }
 });
+
+shoppingCartRouter.patch('/:productId', requireUser, async (req, res, next) => {
+    try {
+        const { productId } = req.params;
+        const { make, model, year, color, isAdmin } = req.body;
+        const product = await getProductById(productId);
+
+        if (req.user.id !== product.userId) {
+            return next({
+                name: 'AdminError',
+                message: 'User is not owner of this Admin'
+            })
+        }
+        
+        const updateProduct = await updateProduct({ id: productId, isAdmin, make, model,
+        year, color })
+        return res.send(updateProduct)
+
+    } catch (error) {
+        return next(error)
+    }
+})
