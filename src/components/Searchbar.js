81caea844f1b4b6817ffb0f8fbd289522f4187bc
Allen McKitrick <mckitricka@gmail.com>
2022-06-04 13:25:19 -0400

    newbranch

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -23,18 +23,9 @@ const Searchbar = () => {
             <select>    
                 <option value='Price'>Price</option>
             </select>
-            <select>    
-                <option value='MinCityMPG'>Min City MPG</option>
-            </select>
-            <select>    
-                <option value='MaxCityMPG'>Max City MPG</option>
-            </select>
-            <select>
-                <option value='MinHwyMPG'>Min Hwy MPG</option>
-            </select>
-            <select>
-                <option value='MaxHwyMPG'>Max Hwy MPG</option>
-            </select>
+        
+         
+        
         </span>
         </div>
         
