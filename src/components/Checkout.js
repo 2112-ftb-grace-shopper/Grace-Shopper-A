ab1e75320d3f58ccdb2a59ad2b4b8598f8fa1dcc
ConnorEndsley <endsleyconnor95@gmail.com>
2022-06-04 23:08:39 -0500

    purchasing a product

---
+++ src/components/Checkout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '../style/MiscAPIpage.css';
+
+const Checkout = () => {
+    // grab the users information off of props
+    const {user}
+    // handle users information when inputting information to a form
+    // compare it to the information that is in our database
+    // if the information matches
+    // return that we have checked out successfully
+    // if not, display an error for whichever field does not match DB
+
+
+    return (
+        <Link to = '/Navbar'>Back</Link>
+    )
+}
+
+export default Checkout
