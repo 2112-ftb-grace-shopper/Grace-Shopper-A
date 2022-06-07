2bf8f1f94d069fafc850ab380b276019db10e0b4
Shane <shanedfuentes@gmail.com>
2022-06-06 21:13:09 -0500

    further UI

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../style/Home.css";
+
+
+const Home = () => {
+    return (
+        <div>
+            <p>If you are a user, please login and browse our wares!</p>
+        <p>If not, please use the Register form in our navigation bar 
+          to create your profile and get started!</p>
+        </div>
+
+    )
+    
+}
+
+export default Home
